d43cbdb55a40ffde7277eae9703e581f0072fc1b
Deeeeivih <59234495+Deeeeivih@users.noreply.github.com>
2020-10-07 14:11:26 -0300

    Modifique la App! atte: Deivih

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.testgit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.testgit.adapter.AdapterTea;
+import com.example.testgit.model.Tea;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_tea);
+        AdapterTea adapterTea = new AdapterTea(this, Tea.teaList(),R.layout.item_tea);
+
+        LinearLayoutManager ln = new LinearLayoutManager(this);
+        ln.setOrientation(RecyclerView.VERTICAL);
+
+        recyclerView.setLayoutManager(ln);
+        recyclerView.setAdapter(adapterTea);
     }
+
+
 }
